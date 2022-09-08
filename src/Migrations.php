09d275c956b7d5771399f5eabbe2a253f8622cb9
Srdan Nikolic <xanatos1337@gmail.com>
2022-09-08 22:15:02 +0200

    Migrations config fix

--- src/Migrations.php
+++ src/Migrations.php
@@ -11,13 +11,15 @@ class Migrations
         $DB = new Database();
         if ($config !== null) {
             $DB->LoadConfig($config);
+        } else {
+            $config = Config::Get('db');
         }
         $db = $DB->db;
 
-        if ($handle = opendir(Config::Get('app_root') . Config::Get('db.migrations_folder'))) {
+        if ($handle = opendir(Config::Get('app_root') . $config['migrations_folder'])) {
             while (false !== ($file = readdir($handle))) {
                 if (substr($file, -4) == '.php') {
-                    require_once(Config::Get('app_root') . Config::Get('db.migrations_folder')  . $file);
+                    require_once(Config::Get('app_root') . $config['migrations_folder']  . $file);
                 }
             }
         }
@@ -29,7 +31,7 @@ class Migrations
             $results = $stmt->get_result();
             $db->query("SET foreign_key_checks = 0");
             while ($table = $results->fetch_assoc()) {
-                $db->query("DROP TABLE `" . mysqli_real_escape_string($db, $table['Tables_in_' . Config::Get('db.database')]) . "`");
+                $db->query("DROP TABLE `" . mysqli_real_escape_string($db, $table['Tables_in_' . $config['database']]) . "`");
             }
             $db->query("SET foreign_key_checks = 1");
             if ($output) {
@@ -38,7 +40,7 @@ class Migrations
         }
 
         $migrations_table_exists = false;
-        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, Config::Get('db.migrations_table')) . "'";
+        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, $config['migrations_table']) . "'";
         $stmt = $db->prepare($query);
         $stmt->execute();
         $results = $stmt->get_result();
@@ -46,11 +48,11 @@ class Migrations
             $migrations_table_exists = true;
         }
         if (!$migrations_table_exists) {
-            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, Config::Get('db.migrations_table')) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, $config['migrations_table']) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
         }
 
         $model_table_exists = false;
-        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, Config::Get('db.model_table')) . "'";
+        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, $config['model_table']) . "'";
         $stmt = $db->prepare($query);
         $stmt->execute();
         $results = $stmt->get_result();
@@ -58,11 +60,11 @@ class Migrations
             $model_table_exists = true;
         }
         if (!$model_table_exists) {
-            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, Config::Get('db.model_table')) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `table` VARCHAR(255) NOT NULL , `field` VARCHAR(255) NOT NULL , `type` VARCHAR(255) NOT NULL , `nullable` INT NOT NULL, `primary_key` int NOT NULL, `related_table` VARCHAR(255) NULL, `related_field` VARCHAR(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, $config['model_table']) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `table` VARCHAR(255) NOT NULL , `field` VARCHAR(255) NOT NULL , `type` VARCHAR(255) NOT NULL , `nullable` INT NOT NULL, `primary_key` int NOT NULL, `related_table` VARCHAR(255) NULL, `related_field` VARCHAR(255) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
         }
 
         $queue_table_exists = false;
-        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, Config::Get('db.queue_table')) . "'";
+        $query = "SHOW TABLES LIKE '" . mysqli_real_escape_string($DB->db, $config['queue_table']) . "'";
         $stmt = $db->prepare($query);
         $stmt->execute();
         $results = $stmt->get_result();
@@ -70,11 +72,11 @@ class Migrations
             $queue_table_exists = true;
         }
         if (!$queue_table_exists) {
-            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, Config::Get('db.queue_table')) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `queue` VARCHAR(255) NOT NULL, `command` VARCHAR(255) NOT NULL , `data` LONGTEXT NOT NULL DEFAULT '0' , `processing` INT NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+            $db->query("CREATE TABLE `" . mysqli_real_escape_string($db, $config['queue_table']) . "` ( `id` INT NOT NULL AUTO_INCREMENT , `queue` VARCHAR(255) NOT NULL, `command` VARCHAR(255) NOT NULL , `data` LONGTEXT NOT NULL DEFAULT '0' , `processing` INT NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
         }
 
         $migration_files = [];
-        if ($handle = opendir(Config::Get('app_root') . Config::Get('db.migrations_folder'))) {
+        if ($handle = opendir(Config::Get('app_root') . $config['migrations_folder'])) {
             while (false !== ($file = readdir($handle))) {
                 if ('.' === $file) continue;
                 if ('..' === $file) continue;
@@ -90,7 +92,7 @@ class Migrations
         foreach ($migration_files as $migration_file) {
             $migration_name = substr($migration_file, 0, -4);
             $migration_resolved = false;
-            $query = "SELECT * FROM `" . mysqli_real_escape_string($db, Config::Get('db.migrations_table')) . "` WHERE `name`=?";
+            $query = "SELECT * FROM `" . mysqli_real_escape_string($db, $config['migrations_table']) . "` WHERE `name`=?";
             $stmt = $db->prepare($query);
             $stmt->execute([$migration_name]);
             $results = $stmt->get_result();
@@ -105,7 +107,7 @@ class Migrations
                 $migration = new $migration_name();
                 $migration->migrate();
 
-                $query = "INSERT INTO `" . mysqli_real_escape_string($db, Config::Get('db.migrations_table')) . "` (`name`) VALUES(?)";
+                $query = "INSERT INTO `" . mysqli_real_escape_string($db, $config['migrations_table']) . "` (`name`) VALUES(?)";
                 $stmt = $db->prepare($query);
                 $stmt->execute([$migration_name]);
             }
